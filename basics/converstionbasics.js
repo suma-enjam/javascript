8b425eae204388f6a91b3304ef0b8afcd5450e8f
suma-enjam <159140060+suma-enjam@users.noreply.github.com>
2024-10-01 17:20:49 +0000

    operations commit

---
+++ basics/converstionbasics.js
@@ -0,0 +1,25 @@
+let score = "ab";
+console.log(typeof score);
+console.log(typeof(score));
+
+let valNum=Number(score);//converting string to number
+console.log(typeof(valNum));
+console.log(valNum);
+//true => 1 
+//false => 0
+//string => NaN
+//null => 0
+
+let islog=1;
+let islogbool=Boolean(islog);
+console.log(islogbool);
+
+
+//1 => true 0=> false
+//"" =>false
+//"suma" => true
+
+let somenum=90;
+let sumnuminstr=String(somenum);
+console.log(sumnuminstr);
+console.log(typeof sumnuminstr);
---
+++ basics/operations.js
@@ -0,0 +1,34 @@
+let v=3
+let x=-v
+console.table([v,x]);
+
+let s1="hello";
+let s2=" hii"
+console.log(s1+s2);
+
+console.log("1"+2) //12
+console.log("1"+2+2)//122
+
+console.log(1+2+"4")//34
+
+
+//weather to convert in to string or number depends on rules 
+//(maybe depending on first type)
+
+console.log(true)//true
+console.log(+true)//1
+//console.log(true+)//error
+console.log(Boolean(""))//0
+console.log(+"");//0
+
+let n1,n2,n3
+n1=n2=n3=5
+console.table([n1,n2,n3]);// 5 5 5 
+
+
+let h=9
+console.log(++h)
+console.log(h++);
+console.log(h);
+console.log(++h);
+console.log(h);
